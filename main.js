aa940fe7b9433ab5eb0f752c3dc4d755ed6885f7
mario <maogaz@uc.cl>
2020-05-17 19:19:37 -0400

    segundo commit

---
+++ main.js
@@ -0,0 +1,64 @@
+
+const socket = io('wss://le-18262636.bitzonte.com', {
+    path: '/stocks'
+});
+
+socket.on('EXCHANGES', (data) => {
+  //console.log(data);
+  x = document.getElementById('stocks');
+  x.innerHTML = data.ticker;
+
+} );
+
+
+dict = {"AAPL": [], "FB": [], "SNAP": [], "IBM": [], "TWTR": []};
+
+socket.on('UPDATE', (data) => {
+  //console.log(data);
+  x = document.getElementById('ticker');
+  x.innerHTML = data.ticker;
+  y = document.getElementById('value');
+  y.innerHTML = data.value;
+  z = document.getElementById('time');
+  z.innerHTML = data.time;
+
+  dict[data.ticker].push([data.time, data.value]);
+  if(data.ticker == "FB"){
+    drawChart(dict[data.ticker]);
+  };
+
+
+  //drawChart(dict[data.ticker]);
+
+
+
+  //para desconectar socket.disconnect()
+  //para conectar socket.connect()
+} );
+
+google.charts.load('current', {'packages':['line']});
+      //google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart(datos) {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'time');
+      data.addColumn('number', 'Value');
+
+
+      data.addRows(
+        datos
+      );
+
+      var options = {
+        chart: {
+          title: 'Grafico'
+        },
+        width: 900,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
