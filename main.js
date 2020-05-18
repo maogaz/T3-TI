5374d5cc0c44fe0876eba62c675db981dde135ea
Marito <maogaz@uc.cl>
2020-05-18 14:14:07 -0400

    me rindo

--- main.js
+++ main.js
@@ -6,20 +6,24 @@ const socket = io('wss://le-18262636.bitzonte.com', {
 
 //dict = {"AAPL": [], "FB": [], "SNAP": [], "IBM": [], "TWTR": []};
 var dict_acciones = {};
-dict_empresas = {};
-var lista_stocks = [1,2,3,4,5];
+var dict_empresas = {};
+var dict_vol_stock = {};
+var datos_tabla_stocks = [];
+
+
 
 socket.emit('STOCKS');
 socket.on("STOCKS", (data) => {
   for (var i = 0; i < data.length; i++) {
     dict_acciones[data[i].ticker] = [];
+    dict_vol_stock[data[i].ticker] = 0;
     var grafico = document.createElement('div');
     console.log(`${data[i].ticker}`);
     grafico.setAttribute("id", `${data[i].ticker}`);
     content.appendChild(grafico);
 	}
   socket.on('UPDATE', (data) => {
-    console.log(data);
+    //console.log(data);
     x = document.getElementById('ticker');
     x.innerHTML = data.ticker;
     y = document.getElementById('value');
@@ -33,7 +37,38 @@ socket.on("STOCKS", (data) => {
       };
     };
   } );
+  for (var key in dict_acciones) {
+    datos_tabla_stocks.push({"name": key, "volume": dict_vol_stock[key]});
+  };
+  socket.on('BUY', (data) => {
+    dict_vol_stock[data.ticker] += data.volume;
+  } );
+  socket.on('SELL', (data) => {
+    dict_vol_stock[data.ticker] += data.volume;
+    console.log(dict_vol_stock[data.ticker]);
+    console.log(dict_vol_stock[data.ticker]);
+  });
+  console.log(datos_tabla_stocks);
+  var tbody = document.getElementById('tbody');
+  var tr = "<tr>";
+  let suma = 0;
+  for (var i = 0; i < datos_tabla_stocks.length; i++) {
+    console.log("volumen", datos_tabla_stocks[i].volume);
+    suma += datos_tabla_stocks[i].volume;
+    console.log("suma", suma);
+    tr += "<td>" + datos_tabla_stocks[i].name + "</td>" + "<td>$" + datos_tabla_stocks[i].volume + "</td></tr>";
+    tbody.innerHTML = tr;
+  };
+  //console.log(datos_tabla_stocks);
+  // let table = document.querySelector("table");
+  // let dataa = Object.keys(datos_tabla_stocks[0]);
+  // generateTableHead(table, dataa);
+  // generateTable(table, datos_tabla_stocks);
+
+
+
 });
+
 console.log(dict_acciones);
 
 
@@ -86,9 +121,32 @@ function drawChart2(datos, etiqueta) {
         chart: {
           title: etiqueta
         },
-        width: 270,
-        height: 150
+        width: 400,
+        height: 300
       };
       var chart = new google.charts.Line(document.getElementById(etiqueta));
       chart.draw(data, google.charts.Line.convertOptions(options));
+}
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data, id) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      text.setAttribute("id", `${id}`);
+      cell.appendChild(text);
+    }
+  }
 }
