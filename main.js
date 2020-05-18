4c5e1dabda8ed399efaeb643c7a49fe2e2c5b8be
Marito <maogaz@uc.cl>
2020-05-18 10:54:28 -0400

    graficos

--- main.js
+++ main.js
@@ -3,64 +3,66 @@ const socket = io('wss://le-18262636.bitzonte.com', {
     path: '/stocks'
 });
 
+
+//dict = {"AAPL": [], "FB": [], "SNAP": [], "IBM": [], "TWTR": []};
+var dict_acciones = {};
+dict_empresas = {};
+var lista_stocks = [1,2,3,4,5];
+
+socket.emit('STOCKS');
+socket.on("STOCKS", (data) => {
+  for (var i = 0; i < data.length; i++) {
+    dict_acciones[data[i].ticker] = [];
+    var grafico = document.createElement('div');
+    console.log(`${data[i].ticker}`);
+    grafico.setAttribute("id", `${data[i].ticker}`);
+    content.appendChild(grafico);
+	}
+  socket.on('UPDATE', (data) => {
+    console.log(data);
+    x = document.getElementById('ticker');
+    x.innerHTML = data.ticker;
+    y = document.getElementById('value');
+    y.innerHTML = data.value;
+    z = document.getElementById('time');
+    z.innerHTML = data.time;
+    dict_acciones[data.ticker].push([data.time, data.value]);
+    for (var key in dict_acciones) {
+      if(data.ticker == key){
+        drawChart2(dict_acciones[data.ticker], key);
+      };
+    };
+  } );
+});
+console.log(dict_acciones);
+
+
+
 function desconectar() {
-    //socket.disconnect()
-    socket.emit("manual-disconnection", socket.id);
-    socket.close();
+  socket.emit("manual-disconnection", socket.id);
+  socket.close();
+  console.log("Socket Closed ");
+}
 
-};
+
 function conectar() {
-    socket.emit("manual-connection", socket.id);
-    socket.open();
-};
+  socket.emit("manual-connection", socket.id);
+  socket.open();
+  console.log("Socket open ");
+}
 
-socket.on('EXCHANGES', (data) => {
-  //console.log(data);
-  x = document.getElementById('stocks');
-  x.innerHTML = data.ticker;
-
-} );
-
-
-dict = {"AAPL": [], "FB": [], "SNAP": [], "IBM": [], "TWTR": []};
-
-socket.on('UPDATE', (data) => {
-  //console.log(data);
-  x = document.getElementById('ticker');
-  x.innerHTML = data.ticker;
-  y = document.getElementById('value');
-  y.innerHTML = data.value;
-  z = document.getElementById('time');
-  z.innerHTML = data.time;
-
-  dict[data.ticker].push([data.time, data.value]);
-  if(data.ticker == "FB"){
-    drawChart(dict[data.ticker]);
-  };
-
-
-  //drawChart(dict[data.ticker]);
-
-
-
-  //para desconectar socket.disconnect()
-  //para conectar socket.connect()
-} );
 
 google.charts.load('current', {'packages':['line']});
       //google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart(datos) {
+function drawChart(datos) {
 
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'time');
       data.addColumn('number', 'Value');
-
-
       data.addRows(
         datos
       );
-
       var options = {
         chart: {
           title: 'Grafico'
@@ -68,8 +70,25 @@ google.charts.load('current', {'packages':['line']});
         width: 900,
         height: 500
       };
-
       var chart = new google.charts.Line(document.getElementById('linechart_material'));
-
       chart.draw(data, google.charts.Line.convertOptions(options));
-    }
+}
+
+function drawChart2(datos, etiqueta) {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'time');
+      data.addColumn('number', 'Value');
+      data.addRows(
+        datos
+      );
+      var options = {
+        chart: {
+          title: etiqueta
+        },
+        width: 270,
+        height: 150
+      };
+      var chart = new google.charts.Line(document.getElementById(etiqueta));
+      chart.draw(data, google.charts.Line.convertOptions(options));
+}
